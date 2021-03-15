db79183a246e3a48f6f75c3fa5c0b296b56e27fc
Yasha <yasha.avramov@yandex.ru>
2021-03-15 12:54:40 +0300

    creating slider

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/prev_arrow.svg"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/next_arrow.svg"</button>'
+      });
+  });
