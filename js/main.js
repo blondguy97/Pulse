b5f51186d1a60ad1e1ad3fb3fb21888e2a321c54
Yasha <yasha.avramov@yandex.ru>
2021-03-20 13:43:07 +0300

    create form

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 300,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/prev_arrow.svg"</button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/next_arrow.svg"</button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/next_arrow.svg"</button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            },
+           
+          ]
       });
   });
